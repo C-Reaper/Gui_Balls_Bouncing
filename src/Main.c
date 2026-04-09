8e52763041d384a8fd2159cc5607bdf7a1671648
codeleaded <gnu.c.reaper@gmail.com>
2026-04-09 23:47:34 +0200

    updated

--- src/Main.c
+++ src/Main.c
@@ -13,10 +13,10 @@
 TransformedView tv;
 
 Vec2 outer_p;
-Vec2 outer_r;
+float outer_r;
 
 Vec2 ball_p;
-Vec2 ball_r;
+float ball_r;
 Vec2 ball_v;
 Vec2 ball_a;
 
@@ -29,10 +29,10 @@ void Setup(AlxWindow* w){
 	);
 
 	outer_p = (Vec2){ 0.0f,0.0f };
-	outer_r = (Vec2){ 1.0f,1.0f };;
+	outer_r = 1.0f;
 
 	ball_p = (Vec2){ 0.0f,0.0f };
-	ball_r = (Vec2){ 0.05f,0.05f };
+	ball_r = 0.05f;
 	ball_v = (Vec2){ 4.0f,0.0f };
 	ball_a = (Vec2){ 0.0f,10.0f };
 
@@ -41,58 +41,50 @@ void Setup(AlxWindow* w){
 void Update(AlxWindow* w){
 	tv.ZoomSpeed = (float)w->ElapsedTime;
 	TransformedView_HandlePanZoom(&tv,window.Strokes,(Vec2){ GetMouse().x,GetMouse().y });
+	TransformedView_Output(&tv,(Vec2){ GetWidth(),GetHeight() });
 
 	ball_v = Vec2_Add(ball_v,Vec2_Mulf(ball_a,w->ElapsedTime));
 	ball_p = Vec2_Add(ball_p,Vec2_Mulf(ball_v,w->ElapsedTime));
 
 	const Vec2 d = Vec2_Sub(ball_p,outer_p);
 	const float dist = Vec2_Mag(d);
-
-	const float or = Vec2_Mag(outer_r);
-	const float br = Vec2_Mag(ball_r);
-	const float l = or - br;
+	const float l = outer_r - ball_r;
 	
 	if(dist > l){
 		ball_v = Vec2_Reflect(Vec2_Perp(d),ball_v);
-		if(Vec2_Mag(ball_r)<or) 	ball_r = Vec2_Mulf(ball_r,1.05f);
-		else 						ball_r = Vec2_Mulf(Vec2_Norm(ball_r),or);
+		if(ball_r<outer_r) 	ball_r = ball_r * 1.05f;
+		else 				ball_r = outer_r;
 
-		ball_p = Vec2_Add(outer_p,Vec2_Mulf(Vec2_Norm(d),or - br));
+		ball_p = Vec2_Add(outer_p,Vec2_Mulf(Vec2_Norm(d),outer_r - ball_r));
 	}
 
 	Clear(BLACK);
-
 	
 	Vec2 bg_p = TransformedView_WorldScreenPos(&tv,outer_p);
-	Vec2 bg_r = TransformedView_WorldScreenLength(&tv,outer_r);
-
-	int op_r = (int)(br * 10000.0f) & 255;
-	int op_g = (int)(br * 1000.0f) & 255;
-	int op_b = (int)(br * 100.0f) & 255;
+	Vec2 bg_r = TransformedView_WorldScreenLength(&tv,(Vec2){ outer_r,outer_r });
+	int op_r = (int)(ball_r * 10000.0f) & 255;
+	int op_g = (int)(ball_r * 1000.0f) & 255;
+	int op_b = (int)(ball_r * 100.0f) & 255;
 	Pixel op_c = Pixel_RGBA(op_r,op_g,op_b,255);
 	Circle_R_Render(WINDOW_STD_ARGS,bg_p,bg_r,op_c);
 
-
 	Vec2 p = TransformedView_WorldScreenPos(&tv,ball_p);
-	Vec2 r = TransformedView_WorldScreenLength(&tv,ball_r);
-	
-	int p_r = (int)(br * 500000.0f) & 255;
-	int p_g = (int)(br * 50000.0f) & 255;
-	int p_b = (int)(br * 5000.0f) & 255;
+	Vec2 r = TransformedView_WorldScreenLength(&tv,(Vec2){ ball_r,ball_r });
+	int p_r = (int)(ball_r * 500000.0f) & 255;
+	int p_g = (int)(ball_r * 50000.0f) & 255;
+	int p_b = (int)(ball_r * 5000.0f) & 255;
 	Pixel p_c = Pixel_RGBA(p_r,p_g,p_b,255);
 	Circle_R_Render(WINDOW_STD_ARGS,p,r,p_c);
 
-
-	String str = String_Format("| Offset: %f,%f - Zoom: %f,%f |",tv.Offset.x,tv.Offset.y,tv.Scale.x,tv.Scale.y);
-	RenderCStrSize(str.Memory,str.size,0,0,WHITE);
-	String_Free(&str);
+	CStr_RenderAlxFontf(WINDOW_STD_ARGS,GetAlxFont(),0.0f,0.0f,WHITE,"Offset: %f,%f",tv.Offset.x,tv.Offset.y);
+	CStr_RenderAlxFontf(WINDOW_STD_ARGS,GetAlxFont(),0.0f,GetAlxFont()->CharSizeY,WHITE,"Scale:  %f,%f",tv.Scale.x,tv.Scale.y);
 }
 void Delete(AlxWindow* w){
     
 }
 
 int main(){
-    if(Create("TT Bouncing Ball",1200,1200,1,1,Setup,Update,Delete))
+    if(Create("Balls Bouncing",1900,1000,1,1,Setup,Update,Delete))
         Start();
     return 0;
 }
